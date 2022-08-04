f27ff623b97acf83ae980b22728646180b224e32
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2022-08-04 12:06:08 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='./icons8-math-64.png' width={64} height={64} alt='calculator icon'/>
+        <p className='title'>Calculator</p>
       </header>
+
+      <footer className="App-footer">
+        <p>Icons from&nbsp;</p>
+        <a href='https://icons8.com' className='icon-ref'>icons8</a>
+      </footer>
     </div>
   );
 }
